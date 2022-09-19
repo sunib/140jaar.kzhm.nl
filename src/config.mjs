dd09caf0373b5c910ef43a6a5bb42b3ee6871e89
Simon Koudijs <simonkoudijs@shipitsmarter.com>
2022-09-19 22:09:15 +0200

    Minor adjustements

--- src/config.mjs
+++ src/config.mjs
@@ -1,11 +1,11 @@
 export const SITE = {
-	name: 'AstroWind',
+	name: 'Lustrumconcert KZHM',
 
 	origin: 'https://astrowind.vercel.app',
 	basePathname: '/',
 
-	title: 'AstroWind — Your website with Astro + Tailwind CSS',
-	description: '🚀 AstroWind is a free and ready to start template to make your website using Astro and Tailwind CSS.',
+	title: 'Zeister harmonie lustrumconcert',
+	description: 'Muziekshow met zandprinses, Lord of the Rings en nog veel meer',
 
 	googleAnalyticsId: false, // or "G-XXXXXXXXXX",
 	googleSiteVerificationId: 'orcPxI47GSa-cRvY11tUe6iGg2IO_RPvnA1q95iEM3M',
